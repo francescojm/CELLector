c2545c065ae4cf8f9f82af08a4704289ef5d6649
Francesco Iorio <francescojm@fi-old-mbp.home>
2020-03-19 11:04:14 +0000

    minor bug fixed in MakeSelection

--- R/CELLector.R
+++ R/CELLector.R
@@ -287,7 +287,7 @@ CELLector.makeSelection<-function(modelMat,n,searchSpace){
 
       ## In a previous version, this line contained the following Bug: modelAccounted[flag]<-modelMatIdx
       ## causing inconsistencies in the signature definition of the selected cell lines table
-      modelAccounted[flag]<-as.numeric(rownames(ModelMat)[modelMatIdx])
+      modelAccounted[flag]<-as.numeric(rownames(modelMat)[modelMatIdx])
 
       flag<-flag+1
 
